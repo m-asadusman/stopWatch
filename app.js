14a98eb695d2dd346c2b26de4fd1250fb37f3837
M. Asad Usman <asadusman199@gmail.com>
2025-10-18 22:47:33 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+var display = document.getElementById('display');
+
+var hours = 0
+var minutes = 0
+var seconds = 0
+var timer = null
+
+function showTime(){
+    var h = hours
+    var m = minutes
+    var s = seconds
+
+    if(h < 10){
+        h = '0'+ h
+    }
+    if(m < 10){
+        m = '0'+ m
+    }
+    if(s < 10){
+        s = '0'+ s
+    }
+    display.textContent = h+' : '+m+' : '+s
+}
+
+function start(){
+    if(timer !== null){
+        return
+    }
+    timer = setInterval(function(){
+        seconds++
+        if(seconds == 60){
+            seconds = 0
+            minutes++
+        }
+        if(minutes == 60){
+            minutes = 0
+            hours++
+        }
+        showTime()
+    },1000)
+}
+
+function stop(){
+    clearInterval(timer)
+    timer = null
+}
+
+function reset(){
+    clearInterval(timer)
+    timer = null
+    hours = 0
+    minutes = 0
+    seconds = 0
+    showTime()
+}
